a29046d75b52e98b7a9bf1a8d13103c18cb2e65f
roberto.alsina2 <roberto.alsina2@f9d28ad2-1052-0410-b5c3-81f59250a6cf>
2008-07-15 21:54:58 +0000

    Mark Pilgrim's feedparser

--- urssus.py
+++ urssus.py
@@ -8,10 +8,13 @@ metadata.bind = "sqlite:///urssus.sqlite"
 metadata.bind.echo = True
 
 class Feed(Entity):
-  htmlUrl = Field(Unicode)
-  title   = Field(Unicode)
-  text    = Field(Unicode)
-  description = Field(Unicode)
+  htmlUrl     = Field(Text)
+  xmlUrl      = Field(Text)
+  title       = Field(Text)
+  text        = Field(Text)
+  description = Field(Text)
+  children    = OneToMany('Feed')
+  parent      = ManyToOne('Feed')
 
 # This is just temporary
 setup_all()
@@ -29,13 +32,29 @@ class MainWindow(QtGui.QMainWindow):
     self.ui=Ui_MainWindow()
     self.ui.setupUi(self)
 
+
+class FeedTreeModel():
+  "A Qt model to display categories/feeds/posts"
+  
+
 if __name__ == "__main__":
   import sys
   # For starters, lets import a OPML file into the DB so we have some data to work with
   from xml.etree import ElementTree
   tree = ElementTree.parse(sys.argv[1])
+  current=None
   for outline in tree.findall("//outline"):
-    if outline.get('xmlUrl'):
-      print outline.get('title')
-
-  
+    xu=outline.get('xmlUrl')
+    if xu:
+      f=Feed(xmlUrl=outline.get('xmlUrl'), 
+             htmlUrl=outline.get('htmlUrl'), 
+             title=outline.get('title'),
+             text=outline.get('text'), 
+             description=outline.get('description')
+             )
+      if current:
+        current.children.append(f)
+        f.parent=current
+    else:
+      current=Feed(text=outline.get('text'))
+    session.flush()
