bac4d5a1a0235f42818819c2cdd29fa574b3ea07
roberto.alsina <roberto.alsina@f9d28ad2-1052-0410-b5c3-81f59250a6cf>
2008-07-19 01:01:06 +0000

    New search widget (firefox-like, hopefully ;-)

--- Ui_main.py
+++ Ui_main.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file '/home/ralsina/Desktop/proyectos/urssus/main.ui'
 #
-# Created: Fri Jul 18 21:22:15 2008
+# Created: Fri Jul 18 21:46:59 2008
 #      by: PyQt4 UI code generator 4.4.2
 #
 # WARNING! All changes made in this file will be lost!
