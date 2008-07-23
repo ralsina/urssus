355494d04bc9e504f9ff9f9b68d280dbb87eaa02
roberto.alsina <roberto.alsina@f9d28ad2-1052-0410-b5c3-81f59250a6cf>
2008-07-23 13:36:44 +0000

    My generic config module

---
+++ trunk/config.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+# Singleton config object
+
+import ConfigParser
+import os
+from demjson import JSON
+
+cfdir=os.path.expanduser('~/.urssus/')
+cfname=os.path.join(cfdir,'config')
+
+def getValue(section,key,default=None):
+    section=section.lower()
+    key=key.lower()
+    try:
+        return JSON().decode(conf.get (section,key))
+    except:
+        return default
+
+def setValue(section,key,value):
+    section=str(section)
+    key=str(key)
+    section=section.lower()
+    key=key.lower()
+    value=JSON().encode(value)
+    try:
+        r=conf.set(section,key,value)
+    except ConfigParser.NoSectionError:
+        conf.add_section(section)
+        r=conf.set(section,key,value)
+    f=open(cfname,'w')
+    conf.write(f)
+    return r
+
+
+class ConfigError(Exception):
+    def __init__(self,modulename,msg):
+        self.modulename=modulename
+        self.msg=msg
+
+
+conf=ConfigParser.SafeConfigParser()
+firstRun=False
+if not os.path.isdir(cfdir):
+    os.mkdir(cfdir)
+if not os.path.isfile(cfname):
+    open(cfname).close()
+    firstRun=True
+f=open(cfname,'r')
+conf.readfp(f)
+f.close()
+
+
+
