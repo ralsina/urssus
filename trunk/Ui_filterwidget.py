9a3109bff696aab6f5fa289030b1c8dfa39d9168
roberto.alsina <roberto.alsina@f9d28ad2-1052-0410-b5c3-81f59250a6cf>
2008-07-23 00:47:04 +0000

    * UI work
    * Skeletons of feed editing functions in urssus.py

--- trunk/Ui_filterwidget.py
+++ trunk/Ui_filterwidget.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file '/home/ralsina/Desktop/proyectos/urssus/filterwidget.ui'
 #
-# Created: Fri Jul 18 22:04:04 2008
+# Created: Tue Jul 22 21:33:03 2008
 #      by: PyQt4 UI code generator 4.4.2
 #
 # WARNING! All changes made in this file will be lost!
