b6ab1cb85f4c7f065d46240bd4b0815255c4a186
roberto.alsina <roberto.alsina@f9d28ad2-1052-0410-b5c3-81f59250a6cf>
2008-07-23 13:21:33 +0000

    Added credits for pretty much everything urssus uses, I think.

--- Ui_about.py
+++ Ui_about.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file '/home/ralsina/Desktop/proyectos/urssus/about.ui'
 #
-# Created: Tue Jul 22 21:33:02 2008
+# Created: Wed Jul 23 10:06:54 2008
 #      by: PyQt4 UI code generator 4.4.2
 #
 # WARNING! All changes made in this file will be lost!
@@ -38,32 +38,22 @@ class Ui_Dialog(object):
         self.label.setGeometry(QtCore.QRect(9,9,462,500))
         self.label.setPixmap(QtGui.QPixmap(":/urssus.jpg"))
         self.label.setObjectName("label")
-        self.label_2 = QtGui.QLabel(Dialog)
-        self.label_2.setGeometry(QtCore.QRect(20,370,441,121))
+        self.textBrowser = QtGui.QTextBrowser(Dialog)
+        self.textBrowser.setGeometry(QtCore.QRect(10,270,461,241))
         palette = QtGui.QPalette()
-        brush = QtGui.QBrush(QtGui.QColor(255,255,255))
+        brush = QtGui.QBrush(QtGui.QColor(226,226,226,180))
         brush.setStyle(QtCore.Qt.SolidPattern)
         palette.setBrush(QtGui.QPalette.Active,QtGui.QPalette.Base,brush)
-        brush = QtGui.QBrush(QtGui.QColor(238,237,237,120))
-        brush.setStyle(QtCore.Qt.SolidPattern)
-        palette.setBrush(QtGui.QPalette.Active,QtGui.QPalette.Window,brush)
-        brush = QtGui.QBrush(QtGui.QColor(255,255,255))
+        brush = QtGui.QBrush(QtGui.QColor(226,226,226,180))
         brush.setStyle(QtCore.Qt.SolidPattern)
         palette.setBrush(QtGui.QPalette.Inactive,QtGui.QPalette.Base,brush)
-        brush = QtGui.QBrush(QtGui.QColor(238,237,237,120))
-        brush.setStyle(QtCore.Qt.SolidPattern)
-        palette.setBrush(QtGui.QPalette.Inactive,QtGui.QPalette.Window,brush)
-        brush = QtGui.QBrush(QtGui.QColor(238,237,237,120))
+        brush = QtGui.QBrush(QtGui.QColor(255,255,255))
         brush.setStyle(QtCore.Qt.SolidPattern)
         palette.setBrush(QtGui.QPalette.Disabled,QtGui.QPalette.Base,brush)
-        brush = QtGui.QBrush(QtGui.QColor(238,237,237,120))
-        brush.setStyle(QtCore.Qt.SolidPattern)
-        palette.setBrush(QtGui.QPalette.Disabled,QtGui.QPalette.Window,brush)
-        self.label_2.setPalette(palette)
-        self.label_2.setAutoFillBackground(True)
-        self.label_2.setTextFormat(QtCore.Qt.RichText)
-        self.label_2.setWordWrap(True)
-        self.label_2.setObjectName("label_2")
+        self.textBrowser.setPalette(palette)
+        self.textBrowser.setFrameShape(QtGui.QFrame.NoFrame)
+        self.textBrowser.setFrameShadow(QtGui.QFrame.Plain)
+        self.textBrowser.setObjectName("textBrowser")
 
         self.retranslateUi(Dialog)
         QtCore.QObject.connect(self.okButton,QtCore.SIGNAL("clicked()"),Dialog.accept)
@@ -72,15 +62,29 @@ class Ui_Dialog(object):
     def retranslateUi(self, Dialog):
         Dialog.setWindowTitle(QtGui.QApplication.translate("Dialog", "Dialog", None, QtGui.QApplication.UnicodeUTF8))
         self.okButton.setText(QtGui.QApplication.translate("Dialog", "&Close", None, QtGui.QApplication.UnicodeUTF8))
-        self.label_2.setText(QtGui.QApplication.translate("Dialog", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+        self.textBrowser.setHtml(QtGui.QApplication.translate("Dialog", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:\'Sans Serif\'; font-size:8pt; font-weight:400; font-style:normal;\">\n"
 "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:12pt; font-weight:600;\">uRSSus</span><span style=\" font-size:12pt;\">: a news aggregator (v0.0.1).</span></p>\n"
 "<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"></p>\n"
 "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-weight:600;\">Author:</span> Roberto Alsina &lt;<a href=\"mailto:ralsina@netmanagers.com.ar\"><span style=\" text-decoration: underline; color:#0000ff;\">ralsina@netmanagers.com.ar</span></a>&gt;</p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-weight:600;\">Icons:</span> Dan Leinir Turthra Jensen</p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-weight:600;\">Bear picture:</span> Bob Jagendorf</p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-weight:600;\">Artwork:</p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-weight:600;\">  Icons:</span> Dan Leinir Turthra Jensen (Reinhardt icon set)</p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-weight:600;\">  Bear picture:</span> Bob Jagendorf</p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Urssus uses many other open source programs and libraries. Here they are, and a big thank you to their generous authors:</p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-weight:600;\">Qt: </span>TrollTech</p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-weight:600;\">Feed Finder and Feed Parser:</span> Mark Pilgrim</p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-weight:600;\">Easylog: </span>Jim Jewett</p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-weight:600;\">OPML:</span> Juri Pakaste</p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-weight:600;\">Python: </span>The Python Software Foundation</p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-weight:600;\">Elixir:</span> Daniel Haus, Jonathan LaCour, G. de Menten &amp; others</p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-weight:600;\">SQLAlchemy: </span>Michael Bayer and contributors</p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-weight:600;\">SQLite:<span style=\" font-weight:400;\"> D. Richard Hipp and others</span></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"></p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
 
 import icons_rc
 
