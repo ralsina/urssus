209bac519f2ddb88ce09ffe03b2fb9a4fb3b112c
roberto.alsina <roberto.alsina@f9d28ad2-1052-0410-b5c3-81f59250a6cf>
2008-07-24 01:39:20 +0000

    * Now feed item updates are async (not sure if it's a good idea, but it's trivial to go back and forth ;-)
    * There seems to be a bug in Qt which makes qtreeview's columns to resize when you hide/show a row
    
      I changed the code so the bug is only triggered on "show only unread feeds" mode, where it can't be avoided 
      (unless I remove the items from the model instead of hiding them?)

--- Ui_searchwidget.py
+++ Ui_searchwidget.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file '/home/ralsina/Desktop/proyectos/urssus/searchwidget.ui'
 #
-# Created: Wed Jul 23 10:48:41 2008
+# Created: Wed Jul 23 22:14:11 2008
 #      by: PyQt4 UI code generator 4.4.2
 #
 # WARNING! All changes made in this file will be lost!
