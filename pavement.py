3ea5c39f7acb269af4d0e3e7a9f4f7c99c8bef59
roberto.alsina <roberto.alsina@f9d28ad2-1052-0410-b5c3-81f59250a6cf>
2008-07-25 12:40:23 +0000

    Remove generated files from the repo.

--- pavement.py
+++ pavement.py
@@ -1,3 +1,5 @@
+import os
+
 options(
     setup=Bunch(
         name="uRSSus",
@@ -6,3 +8,23 @@ options(
         author="Roberto Alsina"
     )
 )
+
+@task
+@needs(['compile_resource','compile_ui','generate_setup', 'minilib', 'setuptools.command.sdist'])
+def sdist():
+  """Overrides sdist to make sure that our setup.py is generated."""
+  pass
+
+@task
+def compile_ui():
+  '''Compile the .ui files using pyuic4'''
+  uidir=os.path.join('urssus', 'ui')
+  for f in os.listdir(uidir):
+    if f.endswith('.ui'):
+      print "Compiling ", f
+      os.system ('pyuic4 %s -o %s'%(os.path.join(uidir, f), os.path.join('urssus', 'Ui_%s.py'%f[:-3])))
+      
+@task
+def compile_resource():
+  '''Compile the icons/images resource file'''
+  os.system ('pyrcc4 %s -o %s'%(os.path.join('urssus','images', 'icons.qrc'), os.path.join('urssus', 'icons_rc.py')))
