168fb64ceab6b232e145a57ea2f2e045df474466
roberto.alsina <roberto.alsina@f9d28ad2-1052-0410-b5c3-81f59250a6cf>
2008-08-06 01:24:34 +0000

    Preparing for 0.2.10

--- trunk/pavement.py
+++ trunk/pavement.py
@@ -13,7 +13,7 @@ options(
           'urssus.GoogleReader.web', 
           'urssus.GoogleReader.web.resolvUrl', 
           ],
-        version="0.2.9",
+        version="0.2.10",
         author="Roberto Alsina",
         author_email='ralsina@netmanagers.com.ar',  
         package_data = {'urssus.templates': ['*.tmpl', '*.css', '*.js'],
