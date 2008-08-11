6f2a8d4bc3856056f36724e92872fd185e3fe344
roberto.alsina <roberto.alsina@f9d28ad2-1052-0410-b5c3-81f59250a6cf>
2008-08-11 12:53:24 +0000

    Added copyright notices on all my source files.

--- trunk/urssus/feedupdater.py
+++ trunk/urssus/feedupdater.py
@@ -1,3 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# uRSSus, a multiplatform GUI news agregator
+# Copyright (C) 2008 Roberto Alsina
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# version 2 as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 from globals import *
 import sys, time, datetime, random
 import elixir
