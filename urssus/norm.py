29986fd1b0df4b9ad00bb1e82946c95c25bca0c8
roberto.alsina <roberto.alsina@f9d28ad2-1052-0410-b5c3-81f59250a6cf>
2009-02-12 20:48:47 +0000

    Licensing info for norm.py

--- urssus/norm.py
+++ urssus/norm.py
@@ -1,5 +1,10 @@
 # -*- coding: utf8 -*-
 
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
 import unicodedata
 
 def _norm_car(car):
