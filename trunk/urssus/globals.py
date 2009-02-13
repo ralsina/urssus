5737ddefa11d6feb95b1b87beb7bcce96b8314a0
roberto.alsina <roberto.alsina@f9d28ad2-1052-0410-b5c3-81f59250a6cf>
2009-02-13 02:22:38 +0000

    Fixed silly updater bug

--- trunk/urssus/globals.py
+++ trunk/urssus/globals.py
@@ -52,7 +52,7 @@ logger.setLevel(logging.INFO)
 if sys.platform<>'win32':
   #create console handler and set level to debug
   ch = logging.StreamHandler()
-  ch.setLevel(logging.DEBUG)
+  ch.setLevel(logging.INFO)
   formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
   ch.setFormatter(formatter)
   #add ch to logger
