3f225d8a9316b80cec2c4ebfffbf270fc7323e3a
roberto.alsina <roberto.alsina@f9d28ad2-1052-0410-b5c3-81f59250a6cf>
2009-04-21 13:33:59 +0000

    PEP8

--- trunk/urssus/config.py
+++ trunk/urssus/config.py
@@ -14,7 +14,8 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
 
 # Singleton config object
 
@@ -23,55 +24,58 @@ import os
 from simplejson import dumps, loads
 
 if os.environ.get('URSSUSDIR', None):
-  cfdir=os.environ['URSSUSDIR']
+    cfdir=os.environ['URSSUSDIR']
 else:
-  cfdir=os.path.join(os.path.expanduser('~'),'.urssus')
-  
-cfname=os.path.join(cfdir,'config')
+    cfdir=os.path.join(os.path.expanduser('~'), '.urssus')
 
-def getValue(section,key,default=None):
-  section=section.lower()
-  key=key.lower()
-  try:
-    return loads(conf.get (section,key))
-  except:
-    return default
+cfname=os.path.join(cfdir, 'config')
 
-def setValue(section,key,value):
-  section=str(section)
-  key=str(key)
-  section=section.lower()
-  key=key.lower()
-  value=dumps(value)
-  try:
-    r=conf.set(section,key,value)
-  except ConfigParser.NoSectionError:
-    conf.add_section(section)
-    r=conf.set(section,key,value)
-  f=open(cfname,'w')
-  conf.write(f)
-  return r
+
+def getValue(section, key, default=None):
+    section=section.lower()
+    key=key.lower()
+    try:
+        return loads(conf.get(section, key))
+    except:
+        return default
+
+
+def setValue(section, key, value):
+    section=str(section)
+    key=str(key)
+    section=section.lower()
+    key=key.lower()
+    value=dumps(value)
+    try:
+        r=conf.set(section, key, value)
+    except ConfigParser.NoSectionError:
+        conf.add_section(section)
+        r=conf.set(section, key, value)
+    f=open(cfname, 'w')
+    conf.write(f)
+    return r
 
 
 class ConfigError(Exception):
-  def __init__(self,modulename,msg):
-    self.modulename=modulename
-    self.msg=msg
+
+    def __init__(self, modulename, msg):
+        self.modulename=modulename
+        self.msg=msg
 
 
 conf=ConfigParser.SafeConfigParser()
 if not os.path.isdir(cfdir):
-  os.mkdir(cfdir)
+    os.mkdir(cfdir)
 
 if not os.path.isfile(cfname):
-  open(cfname, 'w').close()
-f=open(cfname,'r')
+    open(cfname, 'w').close()
+f=open(cfname, 'r')
 conf.readfp(f)
 f.close()
 
 
 '''
-Configuration options definitions, used to build an automatic config dialog 
+Configuration options definitions, used to build an automatic config dialog
 (hackers only, regular users get a regular one later).
 
 Do NOT just use them in the sources, define them
@@ -94,7 +98,8 @@ Definitions are as follow:
 
 ('string',    default, help)
 ('password',  default, help)
-('int',       default, help, min,max) # (min==None is no minimum, and the same for max)
+('int',       default, help, min,max) # (min==None is no minimum, and
+                                         the same for max)
 ('strlist',   default, help)
 ('intlist',   default, help)
 ('bool',      default, help)
@@ -103,23 +108,42 @@ Definitions are as follow:
 '''
 
 options = (
-  ('ui', 
+  ('ui',
     (
-      ('alwaysShowFeed',  ('bool', False, "Always show a link to the post's feed when displaying a post")), 
-      ('hideOnTrayClick', ('bool', True, "Hide the main window when clicking on the tray icon")), 
-      ('startOnTray', ('bool', False, "Don't show the main window on startup")), 
-      ('feedOnTitle', ('bool', False, "Add the feed name to the item title (Example: 'My Blog: what I ate this morning'")), 
+      ('alwaysShowFeed', ('bool', False,
+            "Always show a link to the post's feed when displaying a post")),
+      ('hideOnTrayClick', ('bool', True,
+            "Hide the main window when clicking on the tray icon")),
+      ('startOnTray', ('bool', False,
+            "Don't show the main window on startup")),
+      ('feedOnTitle', ('bool', False,
+            "Add the feed name to the item title (Example: 'My Blog: "\
+            "what I ate this morning'")),
     )
-  ), 
-  ('options', 
+  ),
+  ('options',
     (
-      ('defaultRefresh'      ,  ('int', 1800, "How often feeds should be refreshed by default (in seconds).", 300, None )), 
-      ('maxPostsDisplayed'   ,  ('int', 1000, "Limit the display to this many posts. If set too high, opening 'All Feeds' may take forever", 0, None)), 
-      ('defaultExpiration'   ,  ('int',    7, "How long should articles be kept by default. (In days). You need to restart uRSSus to have an effect.", 0, 9999)), 
-      ('fetchOnStartup'      ,  ('bool', False, "Fetch all feeds on startup.")), 
-      ('showDebugDialog'     ,  ('bool', False, "Show dialog on uncatched exceptions. Only a good idea if you want to help debug uRSSus ;-). You need to restart uRSSus to have an effect.")), 
-      ('followLinksInUrssus' ,  ('bool', False, "If True, links in articles will open in uRSSus window, instead of in your web browser. You can still open in the browser by using right click and 'Open in New Window'")), 
+      ('defaultRefresh'      , ('int', 1800,
+        "How often feeds should be refreshed by default (in seconds).",
+        300, None )),
+      ('maxPostsDisplayed'   , ('int', 1000,
+        "Limit the display to this many posts."\
+        "If set too high, opening 'All Feeds' may take forever", 0, None)),
+      ('defaultExpiration'   , ('int', 7, "How long should articles "\
+                                 "be kept by default. (In days). You need"\
+                                 " to restart uRSSus to have an effect.",
+                                 0, 9999)),
+      ('fetchOnStartup'      , ('bool', False,
+                                 "Fetch all feeds on startup.")),
+      ('showDebugDialog'     , ('bool', False, "Show dialog on uncatched "\
+                                "exceptions. Only a good idea if you want "\
+                                "to help debug uRSSus ;-). You need to "\
+                                "restart uRSSus to have an effect.")),
+      ('followLinksInUrssus' , ('bool', False, "If True, links in articles"\
+                                " will open in uRSSus window, instead of in "\
+                                "your web browser. You can still open in the"\
+                                " browser by using right click and 'Open in "\
+                                "New Window'")),
     )
-  ), 
+  ),
 )
-
