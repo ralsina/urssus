e43016235722e6e2d5b05026b6769a3193646e6f
roberto.alsina <roberto.alsina@f9d28ad2-1052-0410-b5c3-81f59250a6cf>
2009-04-21 13:07:05 +0000

    PEP8

--- urssus/updater.py
+++ urssus/updater.py
@@ -1,10 +1,11 @@
 from urssus.dbtables import *
 initDB()
 for f in Feed.query.all():
-	try:
-		if not f.xmlUrl: continue
-		print f.xmlUrl
-		f.update()
-		f.expire(purge=True)
-	except:
-		pass
+    try:
+        if not f.xmlUrl:
+            continue
+        print f.xmlUrl
+        f.update()
+        f.expire(purge=True)
+    except:
+        pass
